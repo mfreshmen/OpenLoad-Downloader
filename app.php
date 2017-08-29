01bffbdb7b962dc3ea7e100a569f2778d4bbb567
Giuseppe Eletto <Majestcx@users.noreply.github.com>
2017-08-29 02:17:19 +0200

    Update app.php

--- app.php
+++ app.php
@@ -23,30 +23,7 @@ if($video == ''){
 
 	if($response->getStatus() === 200) {
 		$openload = $response->getContent();
-		if(strpos($openload, 'We are sorry!') !== False){
-			echo json_encode(array('error' => '404', 'msg' => 'File not found'));
-			exit();
-		}
-	    	$openload = explode('<span id="streamurl">', $openload)[1];
-	    	$file = 'https://oload.tv/stream/'.explode('</span>', $openload)[0];
-    		$headers = get_headers($file,1);
-    		
-    		//Final Args
-    		$filename = explode('?', end(explode('/',$headers['Location'])))[0];
-	   	$file = explode('?', $headers['Location'])[0];
-	   	$size = $headers['Content-Length'];
-	   	
-	   	//Download Code
-	   	set_time_limit(0);
-	   	header('Content-Type: video/mp4');
-		header('Content-Length: '.$size);
-	   	
-	   	$f = fopen($file, "rb");
-	   	while (!feof($f)) {
-		 	echo fread($f, 8*1024);
-		   	flush();
-		   	ob_flush();
-	   	}
+		echo $openload;
 	   	exit();
 	}else{
 		echo json_encode(array('error' => $response->getStatus(), 'msg' => 'Server error'));
